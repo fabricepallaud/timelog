3d0c61879d7ca7ede9f62019cf873b26a7ddadbf
Fabrice Pallaud <info@baiadesign.com>
2018-11-09 21:21:33 +0000

    Cleaned up NewRowForm.vue

--- src/components/NewRowForm.vue
+++ src/components/NewRowForm.vue
@@ -1,23 +1,4 @@
 <template>
-  <!--
-  <div id="new-row-form" :class="{ active: active }">
-    <h3>New Row</h3>
-    <div>
-      <h4>Project</h4>
-      <select>
-        <option name="projet1" value="Project #1">Project #1</option>
-        <option name="projet2" value="Project #2">Project #2</option>
-      </select>
-    </div>
-    <div>
-      <h4>Task</h4>
-      <select>
-        <option name="task1" value="Task #1">Task #1</option>
-        <option name="task2" value="Task #2">Task #2</option>
-      </select>
-    </div>
-    <button @click="close">Close</button>
-  </div-->
   <div class="text-xs-center">
     <v-dialog
       v-model="dialog"
@@ -86,19 +67,7 @@
       return {
         dialog: false
       }
-    },
-    /*
-    computed: {
-      active() {
-        return this.$store.state.newRowFormActive;
-      }
-    },
-    methods: {
-      close: function() {
-        this.$store.commit('newRowFormActive', false);
-      }
     }
-    */
   }
 </script>
 
